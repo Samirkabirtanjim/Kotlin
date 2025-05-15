88b9e79d58a26a76ea87654f2de302947aca22ab
Samir Kabir Tanjim <122144139+Samirkabirtanjim@users.noreply.github.com>
2025-05-15 18:25:41 +0600

    Q2.kt

---
+++ 1. Conditional Operations/Q2.kt
@@ -0,0 +1,11 @@
+// If a button is clicked more than 3 times, print "Too many clicks". Otherwise, print "Keep clicking".
+fun main(){
+
+    val clicked: Int = 2
+
+    if (clicked >= 3){
+        println("Too many clicks")
+    }else{
+        print("Keep Clicking")
+    }
+}
