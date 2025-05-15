b40a24c92e8b88f55f65653c3a81773bef829b87
Samir Kabir Tanjim <122144139+Samirkabirtanjim@users.noreply.github.com>
2025-05-15 22:53:53 +0600

    Add files via upload

---
+++ LearnDayOne/src/main/kotlin/AgeCheck.kt
@@ -0,0 +1,10 @@
+// Check if a user is 18 or older. If yes, print "Adult", else print "Minor".
+fun main() {
+    val age: Int = 20
+
+    if (age >= 18) {
+        println("Adult")
+    } else {
+        println("Minor")
+    }
+}
---
+++ LearnDayOne/src/main/kotlin/LoginCheck.kt
@@ -0,0 +1,12 @@
+// Check if a username is "admin" and password is "1234". If both are correct, print "Login success", else print "Login failed".
+
+fun main() {
+    val username: String = "admin"
+    val password: Int = 1234
+
+    if (username == "admins" && password == 1234) {
+        println("Login success")
+    } else {
+        println("Login failed")
+    }
+}
