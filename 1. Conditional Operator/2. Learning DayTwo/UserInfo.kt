f98f263978eb46258b0af0d679b9c6ec98500154
Samir Kabir Tanjim <122144139+Samirkabirtanjim@users.noreply.github.com>
2025-05-16 10:44:12 +0600

    UserInfo.kt

---
+++ 1. Conditional Operator/2. Learning DayTwo/UserInfo.kt
@@ -0,0 +1,23 @@
+//🔹 Data to Store:
+//Number of siblings – Byte
+//Year of birth – Short
+//Total study hours per year – Int
+//Phone number – Long
+//Height in meters – Float
+//Weight in kg – Double
+//Blood group initial – Char
+//Learning Kotlin now – Boolean
+
+fun main() {
+
+    val siblings: Byte = 1
+    val yob: Short = 2001
+    val studyPerHour: Int = 2
+    val number: Long = 880L
+    val heightInMeters: Float = 5.10F
+    val weight: Double = 74.0
+    val bloodGroup: String = "O+"
+    val kot: Boolean = true
+
+    println("I have $siblings sibling. My year of birth is $yob. I study $studyPerHour hours daily. My phone number is $number. My height is $heightInMeters meters. I weigh $weight kg. My blood group is $bloodGroup. Learning Kotlin: $kot.")
+}
