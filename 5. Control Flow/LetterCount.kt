5a8860719440163b9ee7392da3ce7780fb9b4ff0
Samir Kabir Tanjim <122144139+Samirkabirtanjim@users.noreply.github.com>
2025-05-24 15:25:49 +0600

    LetterCount.kt

--- 5. Control Flow/LetterCount.kt
+++ 5. Control Flow/LetterCount.kt
@@ -3,5 +3,10 @@ package LearningDay7
 //Count how many times a letter appears
 fun main(){
     val word = "banana"
-    
+    val result = word.groupingBy { it }.eachCount()
+
+    for ((letter, count) in result){
+        println("Letter $letter' appears $count")
+    }
+
 }
