372c0358abee1a9cb6838819ca691bdb6cd8a37c
Samir Kabir Tanjim <122144139+Samirkabirtanjim@users.noreply.github.com>
2025-05-26 01:44:16 +0600

    Add files via upload

---
+++ 5. Control Flow/ListofContacts.kt
@@ -0,0 +1,10 @@
+package LearningDay9
+
+//List of Contacts
+fun main() {
+    val contacts = listOf("Samir", "Amin", "Rahim", "Jarin")
+    for (name in contacts) {
+        println("Contact: $name")
+    }
+}
+
