6c7728176f0bfbed0e9912f9133393af760e83b9
Samir Kabir Tanjim <122144139+Samirkabirtanjim@users.noreply.github.com>
2025-05-27 23:12:19 +0600

    Add files via upload

---
+++ 5. Control Flow/UpdateAValue.kt
@@ -0,0 +1,9 @@
+package LearningDay10Array
+
+//Create an array with 5 numbers and update one of the values.
+
+fun main(){
+    var number = arrayOf(1,2,3,4,50)
+    number[1] = 10
+    println(number[1])
+}
