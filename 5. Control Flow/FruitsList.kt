09b741a40c91f5bd3c731ab8e7db3f27c3fb3f49
Samir Kabir Tanjim <122144139+Samirkabirtanjim@users.noreply.github.com>
2025-05-30 20:18:16 +0600

    Add files via upload

--- 5. Control Flow/FruitsList.kt
+++ 5. Control Flow/FruitsList.kt
@@ -3,5 +3,9 @@ package LearningDay10Array
 //Create a mutable list of 4 fruits. Add one fruit, remove one fruit, and change one fruit.
 
 fun main(){
-    
+    var fruits = mutableListOf("Apple","Banana","Mango","Licchi")
+
+    fruits.add("Grave")
+    fruits.remove("Banana")
+    println(fruits)
 }
