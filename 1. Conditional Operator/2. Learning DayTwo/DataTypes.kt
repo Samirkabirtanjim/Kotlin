862b6f5df2fd8dd0bd24b837f6dfa5ef14329428
Samir Kabir Tanjim <122144139+Samirkabirtanjim@users.noreply.github.com>
2025-07-02 18:12:53 +0600

    Add files via upload

---
+++ 1. Conditional Operator/2. Learning DayTwo/DataTypes.kt
@@ -0,0 +1,22 @@
+package com.shajim.kotlin_programming
+
+fun main(args: Array<String>) {
+
+    var a : Boolean = true
+    var b : Char = 'K'
+    var c : Byte = 12
+    var d : Short = -356
+    var e : Int = 43543
+    var f : Long = -432543234L
+    var g : Float = 3.14F
+    var h : Double = 3.14
+
+    println(a)
+    println(b)
+    println(c)
+    println(d)
+    println(e)
+    println(f)
+    println(g)
+    println(h)
+}
