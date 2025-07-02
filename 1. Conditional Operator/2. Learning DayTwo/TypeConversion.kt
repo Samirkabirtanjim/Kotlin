9143718f4685bd021d52e663582470aa179b2aea
Samir Kabir Tanjim <122144139+Samirkabirtanjim@users.noreply.github.com>
2025-07-02 18:25:28 +0600

    Add files via upload

---
+++ 1. Conditional Operator/2. Learning DayTwo/TypeConversion.kt
@@ -0,0 +1,16 @@
+package com.shajim.kotlin_programming
+
+fun main(){
+
+    var age : Double = 25.6
+
+    var ageInt = age.toInt()
+    println(ageInt)
+
+    var ageDouble = age.toDouble()
+    println(ageDouble)
+
+    var numberString = age.toString()
+    println(numberString)
+
+}
