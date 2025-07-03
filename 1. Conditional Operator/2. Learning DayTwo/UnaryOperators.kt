3b9f432a53b4199025c18f2d2b3e269508c4f875
Samir Kabir Tanjim <122144139+Samirkabirtanjim@users.noreply.github.com>
2025-07-04 00:28:13 +0600

    Add files via upload

---
+++ 1. Conditional Operator/2. Learning DayTwo/UnaryOperators.kt
@@ -0,0 +1,21 @@
+package com.shajim.kotlin_programming
+
+fun main(args: Array<String>) {
+
+    var x = -10
+    println("Number $x, Unary Plus: ${+x}")
+    println("Number $x, Unary Minus: ${-x}")
+
+    var y = 11
+    println("Number $y, Increment Prefix: ${++y}")
+
+    var z = 12
+    println("Number $z, Increment Suffix: ${z++}")
+
+    var a = 13
+    println("Number $a, Decrement Prefix: ${--a}")
+
+    var b = 14
+    println("Number $b, Decrement Suffix: ${b--}")
+
+}
