60623ffcb46411a1b1e96206ce56ca9055869b6f
Samir Kabir Tanjim <122144139+Samirkabirtanjim@users.noreply.github.com>
2025-07-03 23:31:23 +0600

    Add files via upload

---
+++ 1. Conditional Operator/2. Learning DayTwo/AssignmentsOperator.kt
@@ -0,0 +1,25 @@
+package com.shajim.kotlin_programming
+
+fun main(args: Array<String>) {
+
+    var x = 10
+    var y = 5
+    var z = 0
+
+    z += x
+
+    println("z +=x: $z")
+
+    z -= y
+
+    println("z -=y: $z")
+
+    z *= x
+
+    println("z *=x: $z")
+
+    z /= y
+
+    println("z /=y: $z")
+
+}
