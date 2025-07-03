4b325369c882f30762d92f027e82924167ea4809
Samir Kabir Tanjim <122144139+Samirkabirtanjim@users.noreply.github.com>
2025-07-04 00:28:51 +0600

    Add files via upload

---
+++ 1. Conditional Operator/2. Learning DayTwo/Equality&Relational.kt
@@ -0,0 +1,14 @@
+package com.shajim.kotlin_programming
+
+fun main(args: Array<String>) {
+
+    var a : Int = 12
+    var b : Int = 14
+
+    println("$a and $b are equal: ${a == b}")
+    println("$a and $b are not equal: ${a != b}")
+    println("$a less $b? : ${a < b}")
+    println("$b less than equal $a? : ${b <= a}")
+    println("$a greater $b?: ${a > b}")
+    println("$a greater than equal $b?: ${a >= b}")
+}
